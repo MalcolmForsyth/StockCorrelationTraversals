f5ff7f78a64793fbc2a39e52f637de9d193d09ed
Grant Hoey <ghoey2@siebl-0224-24.ews.illinois.edu>
2021-12-04 01:58:14 -0600

    updated main

--- main.cpp
+++ main.cpp
@@ -7,4 +7,7 @@ int main(int argc, char *argv[]) {
     std::cout << ((G.nodes_[1])->ticker_) << std::endl;
     std::cout << ((G.nodes_[2])->ticker_) << std::endl;
     std::cout << ((G.nodes_[3])->ticker_) << std::endl;
+    G.LoadEdges("tests/test_data/test_edges.txt");
+    std::cout << ((G.nodes_[0])->edges_.front()->sr_coeff_) << std::endl;
+    std::cout << ((G.nodes_[0])->edges_.front()->node_2_->ticker_) << std::endl;
 }
