8ca6606316e0f7003215cb9939c4591819c484ce
Grant Hoey <ghoey2@siebl-0224-29.ews.illinois.edu>
2021-12-03 22:31:36 -0600

    started writing the loading of the graph

--- Graph.cpp
+++ Graph.cpp
@@ -3,9 +3,38 @@
 
 void Graph::AddNode(std::string ticker) {
     Node* n = new Node(ticker);
-    nodes_.push_back(n);
+    size_t index = nodes_.size();
+    nodes_.at(index) = n;   
+    ticker_to_node_.insert(std::pair<std::string, Node*>(ticker, n));
 }
 
 Node::Node(std::string ticker) {
     ticker_ = ticker;
+}
+
+Edge::Edge(Node* node_1, Node* node_2, double sr_coeff) {
+    node_1_ = node_1;
+    node_2_ = node_2;
+    sr_coeff_ = sr_coeff;
+}
+
+Graph::AddEdge(Node* node_1, Node* node_2, double sr_coeff) {
+    Edge* e = new Edge(node_1, node_2, sr_coeff);
+    node_1.edges_.push_front(e);
+    node_2.edges_.push_front(e);
+}
+
+
+Graph::LoadNodes(std::string filepath) {
+  std::string line;
+  ifstream myfile (filepath);
+  if (myfile.is_open())
+  {
+    while (getline(myfile,line) )
+    {
+      AddNode(line);
+    }
+    myfile.close();
+  }
+
 }
--- Graph.h
+++ Graph.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <list>
+#include <map>
 
 namespace finalproject {
     class Graph {
